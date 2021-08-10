42cea43d6385e5d9cbd4287b2ed0346615483917
Evgenii Morozov <zheka1101@yandex.ru>
2021-08-10 15:44:48 +0500

    modify get_html

--- test_parsing/async_parser.py
+++ test_parsing/async_parser.py
@@ -11,14 +11,17 @@ async get & fetch_content, multithread parse, save results
 """
 
 
-async def get_html(session: ClientSession, url: str) -> str:
+async def get_html(session: ClientSession, page: int) -> str:
+    url = URL + f"?page={page}"
     async with session.get(url=url, headers=HEADERS) as response:
         if response.status == 200:
             print(f"Получаю данные с {url}")
             return response.text()
 
 
-
+async def fetch_content() -> list[str]:
+    async with ClientSession() as session:
+        response = await session
 
 
 if __name__ == "__main__":
