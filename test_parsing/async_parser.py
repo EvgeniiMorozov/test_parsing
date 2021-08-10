91d2b1235b1962a30250070dac14dbd278aff732
Evgenii Morozov <zheka1101@yandex.ru>
2021-08-10 15:22:26 +0500

    add async func get_html

--- test_parsing/async_parser.py
+++ test_parsing/async_parser.py
@@ -1,7 +1,7 @@
 import asyncio
 import csv
 
-import aiohttp
+from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 from stuff import HEADERS, HOST, URL
 
@@ -11,10 +11,15 @@ async get & fetch_content, multithread parse, save results
 """
 
 
+async def get_html(session: ClientSession, url: str) -> str:
+    async with session.get(url=url, headers=HEADERS) as response:
+        if response.status == 200:
+            print(f"Получаю данные с {url}")
+            return response.text()
 
-def main():
-    pass
+
+
 
 
 if __name__ == "__main__":
-    main()
+    ...
